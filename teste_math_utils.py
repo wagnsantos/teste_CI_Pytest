3f669b9b02a7e822229bdc9fb076f9147d35efc2
Wagner Santos <163296736+wagnsantos@users.noreply.github.com>
2025-04-14 20:10:24 -0300

    Update teste_math_utils.py

--- teste_math_utils.py
+++ teste_math_utils.py
@@ -7,7 +7,7 @@ class TestMathUtils(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
 
     def test_subtract(self):
-        self.assertEqual(subtract(10, 6), 4)
+        self.assertEqual(subtract(10, 5), 5)
         self.assertEqual(subtract(0, 0), 0)
 
 if __name__ == '__main__':
